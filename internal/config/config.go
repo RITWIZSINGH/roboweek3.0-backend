34301a84de202c349622f863973590b7f9e2e85a
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-21 23:43:05 +0530

    created config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,33 @@
 package config
+
+import (
+    "log"
+    "os"
+    "golang.org/x/oauth2"
+    "golang.org/x/oauth2/google"
+    "github.com/joho/godotenv"
+)
+
+type Config struct {
+    GoogleOAuthConfig *oauth2.Config
+}
+
+func NewConfig() *Config {
+    // Load .env file
+    if err := godotenv.Load(); err != nil {
+        log.Printf("Warning: .env file not found")
+    }
+
+    return &Config{
+        GoogleOAuthConfig: &oauth2.Config{
+            ClientID:     os.Getenv("GOOGLE_CLIENT_ID"),
+            ClientSecret: os.Getenv("GOOGLE_CLIENT_SECRET"),
+            RedirectURL:  os.Getenv("GOOGLE_REDIRECT_URL"),
+            Scopes: []string{
+                "https://www.googleapis.com/auth/userinfo.email",
+                "https://www.googleapis.com/auth/userinfo.profile",
+            },
+            Endpoint: google.Endpoint,
+        },
+    }
+}
