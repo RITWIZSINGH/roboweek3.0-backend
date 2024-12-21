0199a45a4ea12acca85cc342ad89751aac36b85c
Ritwiz <madangopalsingh2803@gmail.com>
2024-12-21 23:35:56 +0530

    created user models

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,1 +1,12 @@
 package models
+
+type User struct {
+	ID string `json:"id"`
+	Email string `json:"email"`
+	Name string `json:"name"`
+}
+
+type AuthResponse struct {
+	Token string `json:"token"`
+	User *User `json:"user"`
+}
